9da061a5517ffd3e7ffbd76965713c9ee01281b2
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2023-06-01 21:24:49 +0300

    bindings: golang: WIP

--- gpt4all-bindings/golang/binding.cpp
+++ gpt4all-bindings/golang/binding.cpp
@@ -1,4 +1,5 @@
-#include "../../gpt4all-backend/llmodel_c.h"
+#include "binding.h"
+
 #include "../../gpt4all-backend/llmodel.h"
 #include "../../gpt4all-backend/llama.cpp/llama.h"
 #include "../../gpt4all-backend/llmodel_c.cpp"
@@ -7,7 +8,6 @@
 
 #include "../../gpt4all-backend/llamamodel.h"
 #include "../../gpt4all-backend/gptj.h"
-#include "binding.h"
 #include <cassert>
 #include <cmath>
 #include <cstdio>
@@ -19,7 +19,7 @@
 #include <iostream>
 #include <unistd.h>
 
-void* load_mpt_model(const char *fname, int n_threads) {
+llmodel_model load_mpt_model(const char *fname, int n_threads) {
     // load the model
     auto gptj = llmodel_mpt_create();
 
@@ -31,7 +31,7 @@ void* load_mpt_model(const char *fname, int n_threads) {
     return gptj;
 }
 
-void* load_llama_model(const char *fname, int n_threads) {
+llmodel_model load_llama_model(const char *fname, int n_threads) {
     // load the model
     auto gptj = llmodel_llama_create();
 
@@ -43,7 +43,7 @@ void* load_llama_model(const char *fname, int n_threads) {
     return gptj;
 }
 
-void* load_gptj_model(const char *fname, int n_threads) {
+llmodel_model load_gptj_model(const char *fname, int n_threads) {
     // load the model
     auto gptj = llmodel_gptj_create();
 
@@ -55,69 +55,46 @@ void* load_gptj_model(const char *fname, int n_threads) {
     return gptj;
 }
 
-std::string res = "";
-void * mm;
-
-void gptj_model_prompt( const char *prompt, void *m, char* result, int repeat_last_n, float repeat_penalty, int n_ctx, int tokens, int top_k,
-                            float top_p, float temp, int n_batch,float ctx_erase)
+void gptj_model_prompt(const char *prompt, llmodel_model model, char *result, int repeat_last_n, float repeat_penalty,
+                       int n_ctx, int tokens, int top_k, float top_p, float temp, int n_batch, float ctx_erase)
 {
-    llmodel_model* model = (llmodel_model*) m;
+    std::string res{};
 
-   // std::string res = "";
- 
-    auto lambda_prompt = [](int token_id)  {
-	        return true;
-    };
-
-    mm=model;
-    res="";
-
-    auto lambda_response = [](int token_id, const char *responsechars) {
-        res.append((char*)responsechars);
-        return !!getTokenCallback(mm, (char*)responsechars);
-	};
-	
+    auto lambda_response = [model, &res](std::int32_t, const char *responsechars) -> bool { res.append(responsechars); return !!getTokenCallback(model, responsechars); };
 	auto lambda_recalculate = [](bool is_recalculating) {
 	        // You can handle recalculation requests here if needed
 	    return is_recalculating;
 	};
 
-    llmodel_prompt_context* prompt_context = new llmodel_prompt_context{
-        .logits = NULL,
-        .logits_size = 0,
-        .tokens = NULL,
-        .tokens_size = 0,
-        .n_past = 0,
-        .n_ctx = 1024,
-        .n_predict = 50,
-        .top_k = 10,
-        .top_p = 0.9,
-        .temp = 1.0,
-        .n_batch = 1,
-        .repeat_penalty = 1.2,
-        .repeat_last_n = 10,
-        .context_erase = 0.5
-    };
+    llmodel_prompt_context prompt_ctx{};
 
-    prompt_context->n_predict = tokens;
-    prompt_context->repeat_last_n = repeat_last_n;
-    prompt_context->repeat_penalty = repeat_penalty;
-    prompt_context->n_ctx = n_ctx;
-    prompt_context->top_k = top_k;
-    prompt_context->context_erase = ctx_erase;
-    prompt_context->top_p = top_p;
-    prompt_context->temp = temp;
-    prompt_context->n_batch = n_batch;    
+    prompt_ctx.n_ctx          = 1024;
+    prompt_ctx.n_predict      = 50;
+    prompt_ctx.top_k          = 10;
+    prompt_ctx.top_p          = 0.9f;
+    prompt_ctx.temp           = 1.0f;
+    prompt_ctx.n_batch        = 1;
+    prompt_ctx.repeat_penalty = 1.2f;
+    prompt_ctx.repeat_last_n  = 10;
+    prompt_ctx.context_erase  = 0.5f;
+
+    prompt_ctx.n_predict = tokens;
+    prompt_ctx.repeat_last_n = repeat_last_n;
+    prompt_ctx.repeat_penalty = repeat_penalty;
+    prompt_ctx.n_ctx = n_ctx;
+    prompt_ctx.top_k = top_k;
+    prompt_ctx.context_erase = ctx_erase;
+    prompt_ctx.top_p = top_p;
+    prompt_ctx.temp = temp;
+    prompt_ctx.n_batch = n_batch;
 
     llmodel_prompt(model, prompt,
-                        lambda_prompt,
-                        lambda_response,
-                    lambda_recalculate,
-                    prompt_context );
+                   [](std::int32_t) -> bool { return true; },
+                   lambda_response,
+                   lambda_recalculate,
+                   &prompt_ctx);
 
     strcpy(result, res.c_str()); 
-
-    free(prompt_context);
 }
 
 void gptj_free_model(void *state_ptr) {
--- gpt4all-bindings/golang/binding.h
+++ gpt4all-bindings/golang/binding.h
@@ -1,22 +1,33 @@
+#ifndef GPT4ALL_BINDINGS_GOLANG_BINDING_H_
+#define GPT4ALL_BINDINGS_GOLANG_BINDING_H_
+
+#ifndef __cplusplus
+#include <stdbool.h>
+#else
+#include <cstdbool>
+#endif
+
+#include "../../gpt4all-backend/llmodel_c.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include <stdbool.h>
+llmodel_model load_mpt_model(const char *fname, int n_threads);
 
-void* load_mpt_model(const char *fname, int n_threads);
+llmodel_model load_llama_model(const char *fname, int n_threads);
 
-void* load_llama_model(const char *fname, int n_threads);
+llmodel_model load_gptj_model(const char *fname, int n_threads);
 
-void* load_gptj_model(const char *fname, int n_threads);
-
-void gptj_model_prompt( const char *prompt, void *m, char* result, int repeat_last_n, float repeat_penalty, int n_ctx, int tokens, int top_k,
-                            float top_p, float temp, int n_batch,float ctx_erase);
+void gptj_model_prompt(const char *prompt, llmodel_model model, char *result, int repeat_last_n, float repeat_penalty,
+                       int n_ctx, int tokens, int top_k, float top_p, float temp, int n_batch, float ctx_erase);
 
 void gptj_free_model(void *state_ptr);
 
-extern unsigned char getTokenCallback(void *, char *);
+extern bool getTokenCallback(llmodel_model, const char *);
 
 #ifdef __cplusplus
 }
 #endif
+
+#endif // GPT4ALL_BINDINGS_GOLANG_BINDING_H_
